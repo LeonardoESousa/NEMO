7fc188f203085b417bdc36db9012aadee9bd89ac
LeonardoESousa <leonardo.sousa137@gmail.com>
2021-10-01 21:39:50 +0200

    Improved batch

--- tadf/batch_lx.py
+++ tadf/batch_lx.py
@@ -54,7 +54,7 @@ try:
         queue += 1
         if queue == num or (queue == leftover and batch_num == len(inputs) - leftover):
             newcommand += 'wait'
-            newcommand.replace('MMMM','{:.0f}'.format(num*nproc/newcommand.count('qchem')))
+            newcommand = newcommand.replace('MMMM','{:.0f}'.format(num*nproc/newcommand.count('qchem')))
             with open('cmd_{}_.sh'.format(batch_num), 'w') as q:
                 q.write(newcommand)
             a = subprocess.call(['bash',batch_file, 'cmd.sh'])
